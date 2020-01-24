bb74ebbb3bac5f7600864447c511501fffe98358
Ninad Chemburkar <48809669+ninadchemburkar@users.noreply.github.com>
2020-01-24 04:38:25 -0500

    Fixed minor errors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -54,7 +54,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequireds
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
